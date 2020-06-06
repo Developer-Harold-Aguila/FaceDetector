17d97af61fd2d131a4036eef7c6cbea8408d6844
Developer-Harold-Aguila <64632410+Developer-Harold-Aguila@users.noreply.github.com>
2020-06-06 23:19:13 +0800

    Fix setting set of bouncing box in state

--- src/App.js
+++ src/App.js
@@ -20,34 +20,42 @@ class App extends Component {
   }
 
   calculateBouncingBox = (data) => {
-    console.log(data)
-    const { bottom_row, left_col, right_col, top_row } = data.outputs[0].data.regions[0].region_info.bounding_box
     const image = document.getElementById("inputImage")
     const width  = Number(image.width)
     const height = Number(image.height)
 
-    const bouncingBoxData = {
-      bottom_row: height - bottom_row ,
-      left_col: bottom_row * width,
-      right_col: width - right_col * width,
-      top_row: left_col * height
-    }
+    let bouncingBoxList = []
+    let bouncingBox = []
 
-    return bouncingBoxData
+    data.outputs[0].data.regions.map(regions => {
+          const bottomRow = regions.region_info.bounding_box.bottom_row
+          const rightCol = regions.region_info.bounding_box.right_col
+          const left_colCol = regions.region_info.bounding_box.left_col
+
+          bouncingBox = {
+              bottom_row: height - bottomRow,
+              left_col: bottomRow * width,
+              right_col: width - rightCol * width,
+              top_row: left_colCol * height
+          }
+
+          bouncingBoxList.push(bouncingBox)
+        }
+    )
+    return bouncingBoxList
   }
 
-  setBouncingBox = (bouncingBoxData) => {
+  setBouncingBox = (bouncingBoxList) => {
     this.setState({
-      bouncingBox: bouncingBoxData
+      bouncingBox: bouncingBoxList
     })
 
-    console.log(this.state.bouncingBox)
+    console.log("State BouncingBox Values",this.state.bouncingBox)
   }
 
   onButtonSubmit = () => {
-    console.log("helolll")
     app.models.predict(Clarifai.FACE_DETECT_MODEL,
-          "https://image.shutterstock.com/image-photo/close-photo-cheerful-excited-glad-260nw-789414166.jpg")
+          "https://i.insider.com/57f67e9157540ccb028b537e?width=600&format=jpeg&auto=webp")
           .then( response => this.setBouncingBox(this.calculateBouncingBox(response)))
           .catch(error=> console.log(error))
   }
@@ -59,7 +67,7 @@ class App extends Component {
         <h1>Hello! I'm a face detector! </h1>
         <ImageInput onButtonSubmit={this.onButtonSubmit}/>
         <ImageHolder
-          imgURL="https://image.shutterstock.com/image-photo/close-photo-cheerful-excited-glad-260nw-789414166.jpg"
+          imgURL="https://i.insider.com/57f67e9157540ccb028b537e?width=600&format=jpeg&auto=webp"
           bouncingBox = {this.state.bouncingBox}/>
       </div>
     )
